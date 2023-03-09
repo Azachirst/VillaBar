adc7026fd7c29f3b99064ba7115cedaa5459ce88
Azachirst <108694709+Azachirst@users.noreply.github.com>
2023-03-09 03:43:06 -0300

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+
+    $("#licafeteria").hide();
+    $("#btcafeteria").click(function(){
+        $("#licafeteria").slideToggle();
+    });
+});
+
+$("#lilicuados").hide();
+$("#btlicuados").click(function(){
+    $("#lilicuados").slideToggle();
+});
+
+$("#lifrapuchino").hide();
+$("#btfrapuchino").click(function(){
+    $("#lifrapuchino").slideToggle();
+});
+
+$("#lismoothie").hide();
+$("#btsmoothie").click(function(){
+    $("#lismoothie").slideToggle();
+});
+
+$("#lidesayuno").hide();
+$("#btdesayuno").click(function(){
+    $("#lidesayuno").slideToggle();
+});
+
+$("#libruschettas").hide();
+$("#btbruschettas").click(function(){
+    $("#libruschettas").slideToggle();
+});
+
+$("#lisandwiches").hide();
+$("#btsandwiches").click(function(){
+    $("#lisandwiches").slideToggle();
+});
+
+$("#lipapafritas").hide();
+$("#btpapafritas").click(function(){
+    $("#lipapafritas").slideToggle();
+});
+
+$("#lipizzas").hide();
+$("#btpizzas").click(function(){
+    $("#lipizzas").slideToggle();
+});
+
+$("#lipostres").hide();
+$("#btpostres").click(function(){
+    $("#lipostres").slideToggle();
+});
+
+$("#libebidas").hide();
+$("#btbebidas").click(function(){
+    $("#libebidas").slideToggle();
+});
+
+$("#licervezas").hide();
+$("#btcervezas").click(function(){
+    $("#licervezas").slideToggle();
+});
+
+$("#lidrinks").hide();
+$("#btdrinks").click(function(){
+    $("#lidrinks").slideToggle();
+});
